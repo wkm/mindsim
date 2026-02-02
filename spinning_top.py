65fee011a7283b88e6fbd25c031090178db44487
WKM <wmacura@gmail.com>
2026-02-02 22:13:33 +0000

    Fiddling with mujoco

---
+++ spinning_top.py
@@ -0,0 +1,61 @@
+import mujoco
+import time
+
+tippe_top = """
+<mujoco model="tippe top">
+  <option integrator="RK4"/>
+
+  <asset>
+    <texture name="grid" type="2d" builtin="checker" rgb1=".1 .2 .3"
+     rgb2=".2 .3 .4" width="300" height="300"/>
+    <material name="grid" texture="grid" texrepeat="8 8" reflectance=".2"/>
+  </asset>
+
+  <worldbody>
+    <geom size=".2 .2 .01" type="plane" material="grid"/>
+    <light pos="0 0 .6"/>
+    <camera name="closeup" pos="0 -.1 .07" xyaxes="1 0 0 0 1 2"/>
+    <body name="top" pos="0 0 .02">
+      <freejoint/>
+      <geom name="ball" type="sphere" size=".02" />
+      <geom name="stem" type="cylinder" pos="0 0 .02" size="0.004 .008"/>
+      <geom name="ballast" type="box" size=".023 .023 0.005"  pos="0 0 -.015"
+       contype="0" conaffinity="0" group="3"/>
+    </body>
+  </worldbody>
+
+  <keyframe>
+    <key name="spinning" qpos="0 0 0.02 1 0 0 0" qvel="0 0 0 0 1 200" />
+  </keyframe>
+</mujoco>
+"""
+model = mujoco.MjModel.from_xml_string(tippe_top)
+data = mujoco.MjData(model)
+
+mujoco.mj_resetDataKeyframe(model, data, 0)
+with mujoco.Renderer(model) as renderer:
+    renderer.update_scene(data, camera="closeup")
+
+    duration = 7  # (seconds)
+framerate = 60  # (Hz)
+
+# Simulate and display video.
+frames = []
+mujoco.mj_resetDataKeyframe(model, data, 0)  # Reset the state to keyframe 0
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    start = time.time()
+
+    # Enable wireframe rendering of the entire scene.
+    viewer.user_scn.flags[mujoco.mjtRndFlag.mjRND_WIREFRAME] = 1
+    viewer.sync()
+
+    while viewer.is_running() and time.time() - start < 30:
+      step_start = time.time()
+      mujoco.mj_step(model, data)
+      viewer.align()
+
+      with viewer.lock():
+         viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(data.time%2)
+
+      viewer.sync()
+
